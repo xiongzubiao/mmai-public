0f3fcca1571e876ff19798e2d95a8f0034c08816
Svetly Todorov <svetly.todorov@memverge.com>
2024-08-15 17:42:12 -0700

    nvidia-gpu-operator-setup.sh: remove /dev/null pipe

--- nvidia-gpu-operator-setup.sh
+++ nvidia-gpu-operator-setup.sh
@@ -12,11 +12,16 @@ kubectl get namespace gpu-operator &>/dev/null || kubectl create namespace gpu-o
 
 helm uninstall nvidia-gpu-operator -n gpu-operator
 
+if [[ "$?" -eq 0 ]]; then
+  div
+  log "Helm uninstallation of gpu-operator failed. See setup guide for more details."
+fi
+
 ## Wait until no pods in the gpu-operator namespace
 
 while true; do
   # Get the output of kubectl get pods
-  output=$(kubectl get pods -n gpu-operator 2>/dev/null)
+  output=$(kubectl get pods -n gpu-operator 2>&1)
 
   # Check if the output contains "No resources"
   if [[ $output == *"No resources"* ]]; then
@@ -25,12 +30,20 @@ while true; do
     break
   else
     div
-    log "Pods found in the gpu-operator namespace. Waiting..."
+    log "Pods found in the gpu-operator namespace:"
+    log $output
+    log "Waiting 5s for termination..."
   fi
 
   # Wait for a short interval before checking again
   sleep 5
 done
+
+div
+log_good "Upgrading helm installation..."
+div
+
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 
 div
 log_good "Installing NVIDIA GPU Operator..."
