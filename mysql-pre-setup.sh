fb1e0b654f201b44fce1f60ebaa6244a11f8012d
Svetly Todorov <svetly.todorov@memverge.com>
2024-08-16 10:43:01 -0700

    mysql-pre-setup.sh: Create directory on all nodes via ANSIBLE

--- mysql-pre-setup.sh
+++ mysql-pre-setup.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-# Automatically determine the node name where the script is being run
-NODE_NAME=$(hostname)
+## Ansible playbook for directory creation
 
-# Check if the specified path exists directly on this node, and create it if it doesn't
-if [ ! -d "/mnt/disks/mmai-mysql-billing" ]; then
-    echo "The directory /mnt/disks/mmai-mysql-billing does not exist on this node ($NODE_NAME). Creating it now."
-    sudo mkdir -p /mnt/disks/mmai-mysql-billing
-    if [ $? -ne 0 ]; then
-        echo "Failed to create the directory /mnt/disks/mmai-mysql-billing. Please check permissions."
-        exit 1
-    fi
-    echo "Directory /mnt/disks/mmai-mysql-billing created successfully."
-else
-    echo "Directory exists on $NODE_NAME."
-fi
+PLAYBOOK=mysql-playbook.yaml
+
+cat > $PLAYBOOK <<EOF
+---
+- name: Create directory on all nodes
+  hosts: all
+  become: yes
+  tasks:
+    - name: Ensure directory exists
+      file:
+        path: /mnt/disks/mmai-mysql-billing
+        state: directory
+        mode: '0655'
+EOF
+
+## Run ansible
+
+ansible-playbook -i config/inventory $PLAYBOOK
+
+rm -rf $PLAYBOOK
