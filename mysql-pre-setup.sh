3d22ed9b41da322c1d011ac3131d7d2a6426a006
Svetly Todorov <svetly.todorov@memverge.com>
2024-08-15 18:18:28 -0700

    mysql-pre-setup.sh: add sudo

--- mysql-pre-setup.sh
+++ mysql-pre-setup.sh
@@ -6,7 +6,7 @@ NODE_NAME=$(hostname)
 # Check if the specified path exists directly on this node, and create it if it doesn't
 if [ ! -d "/mnt/disks/mmai-mysql-billing" ]; then
     echo "The directory /mnt/disks/mmai-mysql-billing does not exist on this node ($NODE_NAME). Creating it now."
-    mkdir -p /mnt/disks/mmai-mysql-billing
+    sudo mkdir -p /mnt/disks/mmai-mysql-billing
     if [ $? -ne 0 ]; then
         echo "Failed to create the directory /mnt/disks/mmai-mysql-billing. Please check permissions."
         exit 1
