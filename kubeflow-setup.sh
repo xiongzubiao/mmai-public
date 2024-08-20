e1d5dbea42626e8bc7924a71c6ad9469131c6140
Svetly Todorov <svetly.todorov@memverge.com>
2024-08-19 17:33:50 -0700

    kubeflow-setup: roll forward to v1.8.0

--- kubeflow-setup.sh
+++ kubeflow-setup.sh
@@ -58,15 +58,15 @@ log "Second, installing kubeflow..."
 div
 
 ## Use newer kubeflow
-## sed -i 's/v1.7.0/v1.8.0/g' ./scripts/k8s/deploy_kubeflow.sh
+sed -i 's/v1.7.0/v1.8.0/g' ./scripts/k8s/deploy_kubeflow.sh
 
 ## Set istio gateway to newer version
-## sed -i 's:istio-1-16:istio-1-17:g' ./scripts/k8s/deploy_kubeflow.sh
+sed -i 's:istio-1-16:istio-1-17:g' ./scripts/k8s/deploy_kubeflow.sh
 
 ## Patch deploy_kubeflow with a git clone that checks for success
-cp ./git-clone.sh ./scripts/k8s/git-clone.sh
-cp ./logging.sh ./scripts/k8s/logging.sh
-sed -i '/^source /i\source git-clone.sh' ./scripts/k8s/deploy_kubeflow.sh
-sed -i 's:git clone:git_clone:g' ./scripts/k8s/deploy_kubeflow.sh
+# cp ./git-clone.sh ./scripts/k8s/git-clone.sh
+# cp ./logging.sh ./scripts/k8s/logging.sh
+# sed -i '/^source /i\source git-clone.sh' ./scripts/k8s/deploy_kubeflow.sh
+# sed -i 's:git clone:git_clone:g' ./scripts/k8s/deploy_kubeflow.sh
 
 ./scripts/k8s/deploy_kubeflow.sh
