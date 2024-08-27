020078850461abf18d8394dd13f13323ab692d77
Edwin Yu <edwinyyyu@gmail.com>
2024-08-27 20:54:22 +0000

    Add warnings for dangerous teardown operations

--- mmcai-teardown.sh
+++ mmcai-teardown.sh
@@ -22,6 +22,7 @@ case $remove_mmcai_manager in
     * ) remove_mmcai_manager=false;;
 esac
 
+div
 read -p "Remove MMC.AI Cluster [y/N]:" remove_mmcai_cluster
 case $remove_mmcai_cluster in
     [Yy]* ) remove_mmcai_cluster=true;;
@@ -29,12 +30,16 @@ case $remove_mmcai_cluster in
 esac
 
 if $remove_mmcai_cluster; then
+    div
+    echo_red "Caution: This is dangerous!"
     read -p "Remove all cluster resources (e.g. node groups, departments, projects, workloads) [y/N]:" remove_cluster_resources
     case $remove_cluster_resources in
         [Yy]* ) remove_cluster_resources=true;;
         * ) remove_cluster_resources=false;;
     esac
 
+    div
+    echo_red "Caution: This is dangerous!"
     read -p "Remove billing database [y/N]:" remove_billing_database
     case $remove_billing_database in
         [Yy]* ) remove_billing_database=true;;
@@ -45,6 +50,7 @@ fi
 if $remove_mmcai_manager \
 && $remove_mmcai_cluster; \
 then
+    div
     read -p "Remove MemVerge image pull secrets [y/N]:" remove_memverge_secrets
     case $remove_memverge_secrets in
         [Yy]* ) remove_memverge_secrets=true;;
@@ -58,6 +64,8 @@ if $remove_mmcai_manager \
 && $remove_billing_database \
 && $remove_memverge_secrets; \
 then
+    div
+    echo_red "Caution: This is dangerous!"
     read -p "Remove namespaces [y/N]:" remove_namespaces
     case $remove_namespaces in
         [Yy]* ) remove_namespaces=true;;
@@ -68,18 +76,24 @@ fi
 if $remove_mmcai_manager \
 && $remove_mmcai_cluster; \
 then
+    div
+    echo_red "Caution: This is dangerous!"
     read -p "Remove Prometheus CRDs and namespace [y/N]:" remove_prometheus_crds_namespace
     case $remove_prometheus_crds_namespace in
         [Yy]* ) remove_prometheus_crds_namespace=true;;
         * ) remove_prometheus_crds_namespace=false;;
     esac
 
+    div
+    echo_red "Caution: This is dangerous!"
     read -p "Remove NVIDIA GPU Operator [y/N]:" remove_nvidia_gpu_operator
     case $remove_nvidia_gpu_operator in
         [Yy]* ) remove_nvidia_gpu_operator=true;;
         * ) remove_nvidia_gpu_operator=false;;
     esac
 
+    div
+    echo_red "Caution: This is dangerous!"
     read -p "Remove Kubeflow [y/N]:" remove_kubeflow
     case $remove_kubeflow in
         [Yy]* ) remove_kubeflow=true;;
