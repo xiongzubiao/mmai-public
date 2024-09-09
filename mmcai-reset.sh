215febcf110bce5e12a493508455bb35f392b0c7
Svetly Todorov <svetly.todorov@memverge.com>
2024-09-09 20:34:30 +0000

    mmcai-reset.sh: wrap each step in a function

--- mmcai-reset.sh
+++ mmcai-reset.sh
@@ -57,122 +57,139 @@ if ! $confirm_selection; then
 fi
 
 # Uninstall helm charts
+function uninstall_mmai_charts () {
+    helm uninstall mmcai-cluster -n $NAMESPACE --timeout 1m0s --ignore-not-found
+    if [ $? -ne 0 ]; then
+        div
+        log_bad "MMC.AI cluster helm chart uninstallation failed."
+    fi
 
-helm uninstall mmcai-cluster -n $NAMESPACE --timeout 1m0s --ignore-not-found
-if [ $? -ne 0 ]; then
-    div
-    log_bad "MMC.AI cluster helm chart uninstallation failed."
-fi
-
-helm uninstall mmcai-manager -n $NAMESPACE --timeout 1m0s --ignore-not-found
-if [ $? -ne 0 ]; then
-    div
-    log_bad "MMC.AI manager helm chart uninstallation failed."
-fi
+    helm uninstall mmcai-manager -n $NAMESPACE --timeout 1m0s --ignore-not-found
+    if [ $? -ne 0 ]; then
+        div
+        log_bad "MMC.AI manager helm chart uninstallation failed."
+    fi
+}
 
 # Then, remove namespaces
-
-div
-log "Removing MMC.AI namespaces..."
-kubectl delete ns $NAMESPACE mmcloud-operator-system --ignore-not-found
+function uninstall_mmai_ns () {
+    div
+    log "Removing MMC.AI namespaces..."
+    kubectl delete ns $NAMESPACE mmcloud-operator-system --ignore-not-found
+}
 
 # Remove the gpu-operator.
 # Uninstalling the helm chart may fail -- just keep chugging
 
-div
-log "Removing NVIDIA GPU Operator..."
-kubectl delete crd nvidiadrivers.nvidia.com --ignore-not-found
-kubectl delete crd clusterpolicies.nvidia.com --ignore-not-found
-helm uninstall -n gpu-operator nvidia-gpu-operator --ignore-not-found
-kubectl delete namespace gpu-operator --ignore-not-found
-# NFD
-kubectl delete crd nodefeatures.nfd.k8s-sigs.io --ignore-not-found
-kubectl delete crd nodefeaturerules.nfd.k8s-sigs.io --ignore-not-found
+function uninstall_dependency_charts () {
+    div
+    log "Removing NVIDIA GPU Operator..."
+    kubectl delete crd nvidiadrivers.nvidia.com --ignore-not-found
+    kubectl delete crd clusterpolicies.nvidia.com --ignore-not-found
+    helm uninstall -n gpu-operator nvidia-gpu-operator --ignore-not-found
+    kubectl delete namespace gpu-operator --ignore-not-found
+    # NFD
+    kubectl delete crd nodefeatures.nfd.k8s-sigs.io --ignore-not-found
+    kubectl delete crd nodefeaturerules.nfd.k8s-sigs.io --ignore-not-found
+}
 
 # Remove the gpu-operator and monitoring namespaces
 
-div
-log "Removing dependency namespaces..."
-kubectl delete ns monitoring gpu-operator
+function uninstall_dependency_ns () {
+    div
+    log "Removing dependency namespaces..."
+    kubectl delete ns monitoring gpu-operator
+}
 
-div
-log_good "Removed all MMC.AI installation components. Reinstalling stack..."
+# div
+# log_good "Removed all MMC.AI installation components. Reinstalling stack..."
 
-div
-log "Applying $MMCAI_GHCR_SECRET..."
-kubectl apply -f $MMCAI_GHCR_SECRET
+function reinstall_mmai_secret() {
+    div
+    log "Applying $MMCAI_GHCR_SECRET..."
+    kubectl apply -f $MMCAI_GHCR_SECRET
+    # MMCAI_GHCR_SECRET should have created the mmcai-system and mmcloud-operator-system namespaces
+    kubectl create namespace monitoring
+}
 
-div
-read -p "MySQL database node hostname: " mysql_node_hostname
-read -sp "MySQL root password: " MYSQL_ROOT_PASSWORD
-echo ""
+function reinstall_mysql_secret() {
+    div
+    read -p "MySQL database node hostname: " mysql_node_hostname
+    read -sp "MySQL root password: " MYSQL_ROOT_PASSWORD
+    echo ""
 
-# MMCAI_GHCR_SECRET should have created the mmcai-system and mmcloud-operator-system namespaces
-kubectl get namespace monitoring &>/dev/null || kubectl create namespace monitoring
-
-kubectl -n $NAMESPACE create secret generic mmai-mysql-secret \
-    --from-literal=mysql-root-password=$MYSQL_ROOT_PASSWORD \
-    --from-literal=mysql-password=$MYSQL_ROOT_PASSWORD \
-    --from-literal=mysql-replication-password=$MYSQL_ROOT_PASSWORD
+    kubectl -n $NAMESPACE create secret generic mmai-mysql-secret \
+        --from-literal=mysql-root-password=$MYSQL_ROOT_PASSWORD \
+        --from-literal=mysql-password=$MYSQL_ROOT_PASSWORD \
+        --from-literal=mysql-replication-password=$MYSQL_ROOT_PASSWORD
+}
 
 ## Reinstall the GPU operator
 
-wget -O gpu-operator-values.yaml https://raw.githubusercontent.com/MemVerge/mmc.ai-setup/main/gpu-operator-values.yaml
-wget -O nvidia-gpu-operator-setup.sh https://raw.githubusercontent.com/MemVerge/mmc.ai-setup/main/nvidia-gpu-operator-setup.sh
-chmod +x nvidia-gpu-operator-setup.sh
-./nvidia-gpu-operator-setup.sh
+function reinstall_nvidia_operator () {
+    wget -O gpu-operator-values.yaml https://raw.githubusercontent.com/MemVerge/mmc.ai-setup/main/gpu-operator-values.yaml
+    wget -O nvidia-gpu-operator-setup.sh https://raw.githubusercontent.com/MemVerge/mmc.ai-setup/main/nvidia-gpu-operator-setup.sh
+    chmod +x nvidia-gpu-operator-setup.sh
+    ./nvidia-gpu-operator-setup.sh
+}
 
 ## Reinstall the mysql directory
 
-wget -O mysql-pre-setup.sh https://raw.githubusercontent.com/MemVerge/mmc.ai-setup/main/mysql-pre-setup.sh
-chmod +x mysql-pre-setup.sh
-./mysql-pre-setup.sh
+function reinstall_mysql () {
+    wget -O mysql-pre-setup.sh https://raw.githubusercontent.com/MemVerge/mmc.ai-setup/main/mysql-pre-setup.sh
+    chmod +x mysql-pre-setup.sh
+    ./mysql-pre-setup.sh
+}
 
 ## Reinstall the charts
 
-helm install -n $NAMESPACE mmcai-cluster oci://ghcr.io/memverge/charts/mmcai-cluster \
-    --set billing.database.nodeHostname=$mysql_node_hostname \
-    --debug 2> mmcai-cluster-debug.log
+function reinstall_mmai_charts () {
+    helm install -n $NAMESPACE mmcai-cluster oci://ghcr.io/memverge/charts/mmcai-cluster \
+        --set billing.database.nodeHostname=$mysql_node_hostname \
+        --debug 2> mmcai-cluster-debug.log
 
-helm install -n $NAMESPACE mmcai-manager oci://ghcr.io/memverge/charts/mmcai-manager \
-    --debug 2> mmcai-manager-debug.log
+    helm install -n $NAMESPACE mmcai-manager oci://ghcr.io/memverge/charts/mmcai-manager \
+        --debug 2> mmcai-manager-debug.log
+}
 
 ## Check for our departments and kueue's CRDs
 
-CRDS_PATH='
-    mmcai-cluster/crds/
-    mmcai-cluster/charts/kueue/templates/crd/
-    '
+function verify_installation () {
+    CRDS_PATH='
+        mmcai-cluster/crds/
+        mmcai-cluster/charts/kueue/templates/crd/
+        '
 
-CRDS=$(kubectl get crds | tail -n +2 | awk '{print $1}')
+    CRDS=$(kubectl get crds | tail -n +2 | awk '{print $1}')
 
-helm pull oci://ghcr.io/memverge/charts/mmcai-cluster --version 0.1.0 --untar
+    helm pull oci://ghcr.io/memverge/charts/mmcai-cluster --version 0.1.0 --untar
 
-div
-log "Checking if CRDs have been installed..."
+    div
+    log "Checking if CRDs have been installed..."
 
-## Get the CRDs from those directories -> check if they appear in the CRD list.
+    ## Get the CRDs from those directories -> check if they appear in the CRD list.
 
-for path in $CRDS_PATH; do
-    crd_regex='name: \([a-z]\|-\|[0-9]\)\+\.\(\([a-z]\|-\|[0-9]\)\+\.\?\)\+'
-               # "name:" denotes the start of CRD name
-               # ignore the \, they're escape characters.
-               # this boils down to:
-               #    ([a-z] | - | [0-9])+ . ([a-z] | - | [0-9])+ .?
-               #    alphanum or dash, followed by period, followed by alphanum, last period optional
-    crd_list=$(grep "$crd_regex" "$path" | awk '{print $3}')
-    for crd in $crd_list; do
-        div
-        log "Check CRD $crd:"
+    for path in $CRDS_PATH; do
+        crd_regex='name: \([a-z]\|-\|[0-9]\)\+\.\(\([a-z]\|-\|[0-9]\)\+\.\?\)\+'
+                # "name:" denotes the start of CRD name
+                # ignore the \, they're escape characters.
+                # this boils down to:
+                #    ([a-z] | - | [0-9])+ . ([a-z] | - | [0-9])+ .?
+                #    alphanum or dash, followed by period, followed by alphanum, last period optional
+        crd_list=$(grep "$crd_regex" "$path" | awk '{print $3}')
+        for crd in $crd_list; do
+            div
+            log "Check CRD $crd:"
 
-        kubectl get crd $crd
-        if [ $? -ne 0 ]; then
-            log_bad "Error: CRD $crd is missing"
-        else
-            log_good "OK: CRD $crd is present"
-        fi
+            kubectl get crd $crd
+            if [ $? -ne 0 ]; then
+                log_bad "Error: CRD $crd is missing"
+            else
+                log_good "OK: CRD $crd is present"
+            fi
 
-        # If a CRD is missing, then reset.
-        # TODO
+            # If a CRD is missing, then reset.
+            # TODO
+        done
     done
-done
+}
